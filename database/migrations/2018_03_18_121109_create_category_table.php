7621653a386a9beed661ab64054f00e70c48d892
Your Name <you@example.com>
2018-11-17 19:58:43 +0800

    first commit

--- database/migrations/2018_03_18_121109_create_category_table.php
+++ database/migrations/2018_03_18_121109_create_category_table.php
@@ -15,11 +15,11 @@ class CreateCategoryTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('parent_id');
+            $table->integer('parent_id')->default(0);
             $table->string('name');
             $table->text('description');
             $table->string('url');
-            $table->tinyInteger('status');
+            $table->boolean('status')->default(1);;
             $table->rememberToken();
             $table->timestamps();
         });
